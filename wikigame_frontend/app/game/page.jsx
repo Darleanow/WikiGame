57f12fdefd26f71fcfa9d857aece066ebc1e8abd
Darleanow <enzohugonn@gmail.com>
2024-06-23 21:22:17 +0200

    feat(Styles): Add enhanced wikipedia styles to the game engine

--- wikigame_frontend/app/game/page.jsx
+++ wikigame_frontend/app/game/page.jsx
@@ -1,8 +1,74 @@
+"use client";
+
+import styles from "./styles.module.css";
+import "./globals.css";
+
+import { useState, useEffect, useRef } from "react";
+import { toast } from "react-toastify";
+import GameNavbar from "../components/game_navbar/game_navbar";
+import "react-toastify/dist/ReactToastify.css";
+
 export default function Game() {
+  const [randomArticleContent, setRandomArticleContent] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const isMounted = useRef(false);
+
+  const fetchArticle = async (url) => {
+    setIsLoading(true);
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      if (data.error) {
+        throw new Error(data.error);
+      }
+      setRandomArticleContent(data.content);
+    } catch (error) {
+      console.error("Error fetching article:", error);
+      toast.error("Failed to fetch article. Please try again.");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const fetchRandomArticle = async () => {
+    await fetchArticle("http://localhost:8000/api/get_random_article");
+  };
+
+  const handleLinkClick = async (event) => {
+    const target = event.target;
+
+    if (
+      target.tagName === "A" ||
+      (target.tagName === "IMG" && target.parentElement.tagName === "A")
+    ) {
+      event.preventDefault();
+      const url =
+        target.tagName === "A" ? target.href : target.parentElement.href;
+      const apiUrl = `http://localhost:8000/api/fetch_article?url=${encodeURIComponent(
+        url
+      )}`;
+      await fetchArticle(apiUrl);
+    }
+  };
+
+  useEffect(() => {
+    if (!isMounted.current) {
+      fetchRandomArticle();
+      isMounted.current = true;
+    }
+  }, []);
+
   return (
-    <div>
-      <h1>Game</h1>
-      <p>Game content</p>
-    </div>
+    <>
+      <div className={styles.main_content}>
+        <GameNavbar />
+        <div className={styles.wikipedia_content} onClick={handleLinkClick}>
+          <div
+            dangerouslySetInnerHTML={{ __html: randomArticleContent }}
+            className={styles.article_content}
+          />
+        </div>
+      </div>
+    </>
   );
 }
